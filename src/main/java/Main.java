d3f4511dfacbd1b3eeeafd87ef6d97520bd22f36
prokovAN <prokopova.ann2000@gmail.com>
2019-06-05 02:28:52 +0300

    add file output

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         JsonObjects objects = new JsonObjects();
         try {
             CommandParser commandParser = new CommandParser(".\\src\\main\\resources\\input.txt");
@@ -7,9 +10,13 @@ public class Main {
                 String command = commandParser.next();
                 objects.process(command);
             }
+
+            PrintWriter out = new PrintWriter(".\\src\\main\\resources\\output.json");
+            out.println(objects.toJSON());
+            out.close();
         } catch (Exception exc) {
             System.err.println(exc.getMessage());
         }
-        System.out.println(objects.toJSON());
+
     }
 }
